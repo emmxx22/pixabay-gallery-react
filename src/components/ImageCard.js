7a3913bc58e89f2400b111d4a623f2ae30fda9ab
emmxx22 <59826844+emmxx22@users.noreply.github.com>
2021-03-19 21:46:04 +0100

    --- Auto-Git Commit ---

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,7 +4,7 @@ const ImageCard = ({image}) => {
 const tags = image.tags.split(',');
 
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl">
         <img src={image.webformatURL} alt="" className="w-full"/>
         <div className="px-6 py-4">
             <div className="font-bold text-purple-500 text-xl mb-2">
@@ -27,7 +27,7 @@ const tags = image.tags.split(',');
         </div>
               <div className="px-6 py-4">
                     {tags.map((tag, index)=>(
-                        <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">
+                        <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 animate-pulse">
                       #{tag}
                     </span>
                     ))}
